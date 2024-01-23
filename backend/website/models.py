b053fe1bdee6852db31a5c8ee212fd96c57a836f
crllmbno1206 <crllmbno@gmail.com>
2024-01-24 00:27:15 +0800

    post relationship

--- backend/website/models.py
+++ backend/website/models.py
@@ -10,10 +10,11 @@ class Note(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150 ), unique=True)
     password = db.Column(db.String (150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    posts = db.relationship('Post')
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
