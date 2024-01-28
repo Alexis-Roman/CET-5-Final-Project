4ec4565a179e1b4f6d05dd7d6e7e887731130382
Carl Lambino <veriasdan@gmail.como>
2024-01-28 13:25:09 +0800

    reverted models

--- backend/website/views.py
+++ backend/website/views.py
@@ -54,21 +54,34 @@ def CreatePost():
     if request.method == "POST":
         category = request.form.get('chosenCat')
         title = request.form.get('postTitle')
+        description = request.form.get('postDescription')
+        materials = request.form.get('postMaterial')
+        instruction_title = request.form.get('instructionTitle')
+        instruction_description = request.form.get('stepDescription')
+        reference = request.form.get('postReferences')
+        image = request.files.get('instructionImage')
+        image_filename = save_image(image)
 
-        if len(category) == 0 or len(title) == 0:
+        if not category or len(title) == 0 or len(description) == 0 or len(materials) == 0 or len(instruction_title) == 0 or len(instruction_description) == 0 or len(reference) == 0:
             flash('Please fill up all the required forms', category='error')
         elif len(title) > 70:
             flash('Title reached maximum limit of characters', category='error')
+        elif len(instruction_title) > 70:
+            flash('Instruction title reached maximum limit of characters', category='error')
 
         else:
             new_post = Post(
                 category=category, 
                 title=title,
+                description=description,
+                materials=materials,
+                instruction_title=instruction_title,
+                instruction_description=instruction_description,
                 user_id=current_user.id)
             db.session.add(new_post)
             db.session.commit()
             flash('Post created!', category='success')
-            return redirect(url_for('views.CreatePost'))
+            return redirect(url_for('views.create-post'))
 
     return render_template("Create-Post.html", user=current_user)
 
