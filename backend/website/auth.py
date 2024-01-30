d07453f12086d917abefb1ae7704937bb0b98e72
crllmbno1206 <crllmbno@gmail.com>
2024-01-30 19:55:54 +0800

    fixed auth

--- backend/website/auth.py
+++ backend/website/auth.py
@@ -42,14 +42,14 @@ def signup():
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(username) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+        elif len(email) < 12:
+            flash('Email must be greater than 12 characters.', category='error')
+        elif len(username) < 6:
+            flash('Username too short', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least 8 characters.', category='error')
         else:
             new_user = User(
                 email=email, 
