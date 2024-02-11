69c2dd3c0b83c6ac84091e5f12114cdb1e2f1381
crllmbno1206 <crllmbno@gmail.com>
2024-02-12 00:03:22 +0800

    chito

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -9,8 +9,9 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = 'UPCYCLE_KEY'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['UPLOAD_FOLDER'] = 'backend/website/static/images/UPLOADED IMG'
     db.init_app(app)
 
     from .views import views
@@ -19,7 +20,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User
     
     with app.app_context():
         db.create_all()
--- backend/website/auth.py
+++ backend/website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db  
 from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
@@ -29,39 +29,38 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('views.home'))
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        username = request.form.get('userName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        username = request.form.get('username')
-        birthday = request.form.get('birthday')
 
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif len(username) < 2:
-            flash('Username must be greater than 1 character.', category='error')
-        elif len(birthday) < 2:
-            flash('You must enter birthday!', category='error')
+        elif len(email) < 12:
+            flash('Email must be greater than 12 characters.', category='error')
+        elif len(username) < 6:
+            flash('Username too short', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be at least 8 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='scrypt'))
+            new_user = User(
+                email=email, 
+                username=username, 
+                password=generate_password_hash(
+                password1, 
+                method='scrypt'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
+            return redirect(url_for('views.login'))
         
     return render_template("Signup.html", user=current_user)
